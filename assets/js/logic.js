69ee46540553050def5049205024317dd5a225a0
Claire Price <claire.price@mail.com>
2023-12-29 14:20:31 +0000

    put questions in an array

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -85,14 +85,60 @@ var timer = setInterval(function() {
 //}
 
 
+// var startScreen = document.getElementById('start-screen');
+// var startButton = document.getElementById('start');
+// var question = document.getElementById('question-title');
+// var startMode = document.querySelector('.start');
+// var questionMode = document.querySelector('.hide');
+// var choicesMode = document.querySelector('.choices');
+
+// // Create buttons
+// var optionsButton1 = document.createElement('button');
+// document.getElementById('choices').appendChild(optionsButton1);
+// var optionsButton2 = document.createElement('button');
+// document.getElementById('choices').appendChild(optionsButton2);
+// var optionsButton3 = document.createElement('button');
+// document.getElementById('choices').appendChild(optionsButton3);
+// var optionsButton4 = document.createElement('button');
+// document.getElementById('choices').appendChild(optionsButton4);
+
+
+// function showQuestion(questionNumber) {
+//       questionMode.style.display = 'block';
+//       question.innerHTML = questionNumber.question;
+
+//       optionsButton1.innerHTML = questionNumber.answerOptions[0];
+//       optionsButton2.innerHTML = questionNumber.answerOptions[1];
+//       optionsButton3.innerHTML = questionNumber.answerOptions[2];
+//       optionsButton4.innerHTML = questionNumber.answerOptions[3];
+
+//       choicesMode.addEventListener('click', function(){
+//         questionMode.style.display = 'none';
+//         showQuestion(question2);
+//         });
+// }
+
 
 
 // // Start quiz
-// startButton.addEventListener('click', function(){
-   
-//   startMode.style.display = 'none';
-//   runQuestion(question1);
-//   runQuestion(question2);
+// function game() {
+//   startButton.addEventListener('click', function(){
+//     startMode.style.display = 'none';
+//     questions.forEach(showQuestion);
+//   });
+  
+// }
+
+// game();
+
+// choices.addEventListener('click', function(){
+//     if (mode === 'hide') {
+//         mode = 'unhide';
+//         questions.setAttribute('class', 'unhide');
+//     } else {
+//         mode = 'hide';
+//         questions.setAttribute('class', 'hide');
+//     }
 // });
 
 
@@ -113,56 +159,31 @@ document.getElementById('choices').appendChild(optionsButton3);
 var optionsButton4 = document.createElement('button');
 document.getElementById('choices').appendChild(optionsButton4);
 
+
 function showQuestion(questionNumber) {
       questionMode.style.display = 'block';
       question.innerHTML = questionNumber.question;
+
       optionsButton1.innerHTML = questionNumber.answerOptions[0];
       optionsButton2.innerHTML = questionNumber.answerOptions[1];
       optionsButton3.innerHTML = questionNumber.answerOptions[2];
       optionsButton4.innerHTML = questionNumber.answerOptions[3];
+
       choicesMode.addEventListener('click', function(){
         questionMode.style.display = 'none';
         showQuestion(question2);
         });
-      
-      // optionsButton2.addEventListener('click', function(){
-      //   questionMode.style.display = 'none';
-      // });
-      // optionsButton3.addEventListener('click', function(){
-      //   questionMode.style.display = 'none';
-      // });
-      // optionsButton4.addEventListener('click', function(){
-      //   questionMode.style.display = 'none';
-      // });
 }
 
 
 
-
-// optionsButton1.addEventListener('click', function(){
-//   questionMode.style.display = 'none';
-// });
-
-
 // Start quiz
-
-
 function game() {
   startButton.addEventListener('click', function(){
     startMode.style.display = 'none';
-    showQuestion(question1);
+    questions.forEach(showQuestion);
   });
   
 }
 
 game();
-
-// choices.addEventListener('click', function(){
-//     if (mode === 'hide') {
-//         mode = 'unhide';
-//         questions.setAttribute('class', 'unhide');
-//     } else {
-//         mode = 'hide';
-//         questions.setAttribute('class', 'hide');
-//     }
-// });
