cf6484df5027c9f18e9eb4ce48e1a7de72392fb6
Claire Price <claire.price@mail.com>
2023-12-30 00:16:58 +0000

    add result checking function

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,13 +1,4 @@
 
-var count = 60;
-var timer = setInterval(function() {
-  count--;
-  console.log(count);
-  if (count === 0) {
-    clearInterval(timer);
-    console.log("Time's up!");
-  }
-}, 1000);
 
 
 
@@ -142,48 +133,139 @@ var timer = setInterval(function() {
 // });
 
 
+
+
+
+
+
+// var optionsButton = document.createElement('button');
+// document.getElementById('choices').appendChild(optionsButton);
+
+
+// function addQuestion(questions) {
+//   questionMode.style.display = 'block';
+//   questionTitle.innerHTML = questions.question;
+//   optionsButton1.innerHTML = questions.answerOptions[0];
+//   optionsButton2.innerHTML = questions.answerOptions[1];
+//   optionsButton3.innerHTML = questions.answerOptions[2];
+//   optionsButton4.innerHTML = questions.answerOptions[3];
+// }
+
+
+
+  
+
+// var elementClicked = false;
+
+// // Start quiz
+  
+
+// // Question 1
+// function addQuestion(questions) {
+//   for (var i=0; i < questions.length; i++) {
+//   questionMode.style.display = 'block';
+//   questionTitle.innerHTML = questions.question;
+//   optionsButton1.innerHTML = questions.answerOptions[0];
+//   optionsButton2.innerHTML = questions.answerOptions[1];
+//   optionsButton3.innerHTML = questions.answerOptions[2];
+//   optionsButton4.innerHTML = questions.answerOptions[3];
+//   if (elementClicked) continue;
+//   }
+ 
+// }
+  
+
+
+  
+
+  // addClick(addQuestion(question1));
+  // addClick(addQuestion(question2));
+
 var startScreen = document.getElementById('start-screen');
 var startButton = document.getElementById('start');
-var question = document.getElementById('question-title');
 var startMode = document.querySelector('.start');
+
 var questionMode = document.querySelector('.hide');
-var choicesMode = document.querySelector('.choices');
-
-// Create buttons
-var optionsButton1 = document.createElement('button');
-document.getElementById('choices').appendChild(optionsButton1);
-var optionsButton2 = document.createElement('button');
-document.getElementById('choices').appendChild(optionsButton2);
-var optionsButton3 = document.createElement('button');
-document.getElementById('choices').appendChild(optionsButton3);
-var optionsButton4 = document.createElement('button');
-document.getElementById('choices').appendChild(optionsButton4);
+questionMode.style.display = 'none';
 
 
-function showQuestion(questionNumber) {
-      questionMode.style.display = 'block';
-      question.innerHTML = questionNumber.question;
 
-      optionsButton1.innerHTML = questionNumber.answerOptions[0];
-      optionsButton2.innerHTML = questionNumber.answerOptions[1];
-      optionsButton3.innerHTML = questionNumber.answerOptions[2];
-      optionsButton4.innerHTML = questionNumber.answerOptions[3];
 
-      choicesMode.addEventListener('click', function(){
-        questionMode.style.display = 'none';
-        showQuestion(question2);
-        });
+
+
+var currentQuestion = 0;
+var correctAnswers = 0;
+
+
+
+
+
+// Create result paragraph;
+var resultMessage = document.createElement('p');
+resultMessage.id = 'result';
+document.getElementById('questions').appendChild(resultMessage);
+
+
+// Start button click
+startButton.addEventListener('click', function(){
+  startMode.style.display = 'none';
+});
+
+
+function showQuestion() {
+  questionMode.style.display = 'block';
+  var questionTitle = document.getElementById('question-title');
+  questionTitle.innerHTML = questions[currentQuestion].question;
+
+  // Create buttons
+  var optionsButton1 = document.createElement('button');
+  document.getElementById('choices').appendChild(optionsButton1);
+  optionsButton1.innerHTML = questions[currentQuestion].answerOptions[0];
+  optionsButton1.addEventListener('click', function(){
+    checkAnswer(0);
+  }); 
+  var optionsButton2 = document.createElement('button');
+  document.getElementById('choices').appendChild(optionsButton2);
+  optionsButton2.innerHTML = questions[currentQuestion].answerOptions[1];
+  optionsButton2.addEventListener('click', function(){
+    checkAnswer(1);
+  }); 
+  var optionsButton3 = document.createElement('button');
+  document.getElementById('choices').appendChild(optionsButton3);
+  optionsButton3.innerHTML = questions[currentQuestion].answerOptions[2];
+  optionsButton3.addEventListener('click', function(){
+    checkAnswer(2);
+  }); 
+  var optionsButton4 = document.createElement('button');
+  document.getElementById('choices').appendChild(optionsButton4);
+  optionsButton4.innerHTML = questions[currentQuestion].answerOptions[3];
+  optionsButton4.addEventListener('click', function(){
+    checkAnswer(3);
+  }); 
+
+  var result = document.getElementById('result');
+  result.innerHTML = '';
 }
 
-
-
-// Start quiz
-function game() {
-  startButton.addEventListener('click', function(){
-    startMode.style.display = 'none';
-    questions.forEach(showQuestion);
-  });
-  
+function checkAnswer(selected) {
+  var result = document.getElementById('result');
+  if (selected === questions[currentQuestion].correctAnswer) {
+    result.innerHTML = 'Correct!';
+  } else {
+    result.innerHTML = 'Incorrect!';
+  }
 }
 
-game();
+var count = 60;
+var timer = setInterval(function() {
+  count--;
+  console.log(count);
+  if (count === 0) {
+    clearInterval(timer);
+    console.log("Time's up!");
+  }
+}, 1000);
+
+
+
+showQuestion();
