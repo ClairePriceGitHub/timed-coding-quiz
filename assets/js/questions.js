e7b24ae5674958a3187c994e14fc1adfc082baf6
Claire Price <claire.price@mail.com>
2023-12-30 02:51:36 +0000

    update questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,28 +1,28 @@
 var questions = [
     {
-    question: 'what is your favourite colour',
-    answerOptions: ['red', 'orange', 'green', 'blue'],
-    correctAnswer: 'green'
+    question: 'Which of the following methods can be used to display data in some form using Javascript?',
+    answerOptions: ['console.log()', 'window.push()', 'document.message()', 'body.log()'],
+    correctAnswer: 0
     },
     {
-    question: 'what is your favourite animal',
-    answerOptions: ['cat', 'dog', 'fish', 'bird'],
-    correctAnswer: 'cat'
+    question: 'How can a datatype be declared to be a constant type?',
+    answerOptions: ['var', 'const', 'let', 'constant'],
+    correctAnswer: 1
     },
     {
-    question: 'v',
-    answerOptions: ['cat', 'dog', 'fish', 'bird'],
-    correctAnswer: 'cat'
+    question: 'Which of the following methods is used to access HTML elements using Javascript?',
+    answerOptions: ['getTypeElement()', 'getHTMLByElement()', 'getElementByType()', 'getElementById()'],
+    correctAnswer: 3
     },
     {
-    question: 'what is your favourite animal',
-    answerOptions: ['cat', 'dog', 'fish', 'bird'],
-    correctAnswer: 'cat'
+    question: 'When an operator’s value is NULL, the typeof returned by the unary operator is:',
+    answerOptions: ['boolean', 'object', 'undefined', 'integer'],
+    correctAnswer: 1
     },
     {
-    question: 'what is your favourite animal',
-    answerOptions: ['cat', 'dog', 'fish', 'bird'],
-    correctAnswer: 'cat'
+    question: 'Which of the following is not a Javascript framework?',
+    answerOptions: ['vue', 'node', 'drum', 'react'],
+    correctAnswer: 2
     }
 ]
     
