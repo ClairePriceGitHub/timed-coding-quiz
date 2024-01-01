03ca9674e77b204e74cc8f9704bc263fb25a0ae0
Claire Price <claire.price@mail.com>
2024-01-01 19:56:26 +0000

    remove commented out questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,60 +1,34 @@
 var questions = [
     {
-    question: 'Which of the following methods can be used to display data in some form using Javascript?',
+    questionNumber: 1,
+    question: '1. Which of the following methods can be used to display data in some form using Javascript?',
     answerOptions: ['console.log()', 'window.push()', 'document.message()', 'body.log()'],
     correctAnswer: 0
     },
     {
-    question: 'How can a datatype be declared to be a constant type?',
+    questionNumber: 2,
+    question: '2. How can a datatype be declared to be a constant type?',
     answerOptions: ['var', 'const', 'let', 'constant'],
     correctAnswer: 1
     },
     {
-    question: 'Which of the following methods is used to access HTML elements using Javascript?',
+    questionNumber: 3,
+    question: '3. Which of the following methods is used to access HTML elements using Javascript?',
     answerOptions: ['getTypeElement()', 'getHTMLByElement()', 'getElementByType()', 'getElementById()'],
     correctAnswer: 3
     },
     {
-    question: 'When an operator’s value is NULL, the typeof returned by the unary operator is:',
+    questionNumber: 4,
+    question: '4. When an operators value is NULL, the typeof returned by the unary operator is:',
     answerOptions: ['boolean', 'object', 'undefined', 'integer'],
     correctAnswer: 1
     },
     {
-    question: 'Which of the following is not a Javascript framework?',
+    questionNumber: 5,
+    question: '5. Which of the following is not a Javascript framework?',
     answerOptions: ['vue', 'node', 'drum', 'react'],
     correctAnswer: 2
     }
 ]
     
 
-
-
-// var question1 = {
-//     question: 'what is your favourite colour',
-//     answerOptions: ['red', 'orange', 'green', 'blue'],
-//     correctAnswer: 'green'
-//     }
-
-// var question2 = {
-//     question: 'what is your favourite animal',
-//     answerOptions: ['cat', 'dog', 'fish', 'bird'],
-//     correctAnswer: 'cat'
-//     }
-
-// var question3 = {
-//     question: 'v',
-//     answerOptions: ['cat', 'dog', 'fish', 'bird'],
-//     correctAnswer: 'cat'
-//     }
-
-// var question4 = {
-//     question: 'what is your favourite animal',
-//     answerOptions: ['cat', 'dog', 'fish', 'bird'],
-//     correctAnswer: 'cat'
-//     }
-
-// var question5 = {
-//     question: 'what is your favourite animal',
-//     answerOptions: ['cat', 'dog', 'fish', 'bird'],
-//     correctAnswer: 'cat'
-//     }
