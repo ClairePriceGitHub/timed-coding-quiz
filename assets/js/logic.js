07eb86ddbc2796a7fa39bd849647e9e050d52ab5
Claire Price <claire.price@mail.com>
2024-01-01 20:05:03 +0000

    update comments

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -5,24 +5,26 @@ var questionTitle = document.getElementById('question-title');
 var endScreenMode = document.getElementById('end-screen');
 var finalScore = document.getElementById('final-score');
 var submitButton = document.getElementById('submit');
-//var highScores = document.getElementById('highscores');
 var timer = document.querySelector('.timer');
 var time = document.getElementById('time');
 time.innerHTML = 60; // Set timer to start at 60
 
-
 // Create buttons
 var optionsButton1 = document.createElement('button');
 optionsButton1.id = 'optionsButton1';
+optionsButton1.className = 'optionsButtons';
 document.getElementById('choices').appendChild(optionsButton1);
 var optionsButton2 = document.createElement('button');
 optionsButton2.id = 'optionsButton2';
+optionsButton2.className = 'optionsButtons';
 document.getElementById('choices').appendChild(optionsButton2);
 var optionsButton3 = document.createElement('button');
 optionsButton3.id = 'optionsButton3';
+optionsButton3.className = 'optionsButtons';
 document.getElementById('choices').appendChild(optionsButton3);
 var optionsButton4 = document.createElement('button');
 optionsButton4.id = 'optionsButton4';
+optionsButton4.className = 'optionsButtons';
 document.getElementById('choices').appendChild(optionsButton4);
 
 // Create result paragraph
@@ -32,15 +34,10 @@ document.getElementById('questions').appendChild(resultParagraph);
 var result = document.getElementById('result');
 result.innerHTML = '';
 
-// Create high scores list
-
-
 var count = 60;
 var currentQuestion = 0;
 var score = 0;
-var scoreCorrect = 10;
-var scoreIncorrect = -10;
-
+var index = 0;
 
 function startQuiz() {
   // Start button click: hide start information / unhide questions
@@ -48,10 +45,10 @@ function startQuiz() {
     startMode.style.display = 'none';
     questionMode.style.display = 'block';
     // Start timer
-    var timer = setInterval(function() {
+    var countdownTimer = setInterval(function() {
       count--;
       if (count === 0) {
-        clearInterval(timer);
+        clearInterval(countdownTimer);
         alert('You are out of time!');
         location.reload()
       } else {
@@ -66,7 +63,7 @@ function startQuiz() {
 function showQuestion(currentQuestion) {
   result.style.display = 'none';
   questionTitle.innerHTML = questions[currentQuestion].question;
-  // Append buttons
+  // Add text to buttons
   optionsButton1.innerHTML = questions[currentQuestion].answerOptions[0];
   optionsButton2.innerHTML = questions[currentQuestion].answerOptions[1];
   optionsButton3.innerHTML = questions[currentQuestion].answerOptions[2];
@@ -89,23 +86,19 @@ function showQuestion(currentQuestion) {
   });
 }
 
-
 function checkAnswer(index, currentQuestion) {
   if (index === questions[currentQuestion].correctAnswer) {
     result.innerHTML = 'Correct!';
-    score = score + scoreCorrect;
-    console.log(score);
+    score += 50;
   } else {
     result.innerHTML = 'Incorrect!';
-    score = score + scoreIncorrect;
-    console.log(score);
+    score -= 10;
   }
   // Wait 2 seconds before moving to next question so the result can be read
   setTimeout(() => {
     nextQuestion(currentQuestion);
   }, 2000);
 }
-
 
 function nextQuestion(currentQuestion) {
   if (currentQuestion < questions.length - 1) {
@@ -116,7 +109,6 @@ function nextQuestion(currentQuestion) {
   showQuestion(currentQuestion);
 }
 
-
 function endScreen() {
   questionMode.style.display = 'none';
   timer.style.display = 'none';
@@ -126,6 +118,5 @@ function endScreen() {
     logHighScore();
   });
 }
-
 
 startQuiz();
